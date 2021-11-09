59cd33766e582b6f6d6d53e475fc8c916512e7d3
Giuseppe0197 <faro.g@hotmail.it>
2021-11-09 14:09:59 +0100

    impostazione lavoro

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+/* Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+/* andiamo a stampare in pagina 5 numeri casuli generati dal computer */
+
+/* andiamo a creare una funzione che dopo 30 secondi fa sparire il div */
+
+/* creiamo un ciclo per i prompt per chiedere all'utente di inserire i numeri che ha visto generarsi */
+
+/* se i numeri generati sono uguali a quelli del pc allora li aggiungiamo in pagina e controlliamo quanti ne ha indovinati */
